7b9d1d51fb2dc163a570e0ae23adad66e5c55eb0
Donghee Park <dongheepark@gmail.com>
2024-07-30 13:13:41 +0900

    Add web ui

---
+++ demo-server.py
@@ -0,0 +1,86 @@
+import os
+import yaml
+
+import imageio
+import cv2
+from moviepy.editor import *
+from skimage.transform import resize
+from skimage import img_as_ubyte
+
+from demo import load_checkpoints, make_animation, find_best_frame
+import gradio as gr
+
+config = '/checkpoint/vox-256-spade.yaml'
+checkpoint = '/checkpoint/ckpt/00000189-checkpoint.pth.zip'
+
+gen = 'spade'
+cpu = False
+
+generator, kp_detector, he_estimator = load_checkpoints(config_path=config, checkpoint_path=checkpoint, gen=gen, cpu=cpu)
+def inference(source,
+              driving,
+              find_best_frame_ = False,
+              free_view = False,
+              yaw = None,
+              pitch = None,
+              roll = None,
+              output_name = 'output.mp4',
+              
+              audio = True,
+              cpu = False,
+              best_frame = None,
+              relative = True,
+              adapt_scale = True,
+              ):
+
+    # source 
+    source_image = resize(source, (256, 256))
+    
+    # driving
+    reader = imageio.get_reader(driving)
+    fps = reader.get_meta_data()['fps']
+    driving_video = []
+    try:
+        for im in reader:
+            driving_video.append(im)
+    except RuntimeError:
+        pass
+    reader.close()
+
+    driving_video = [resize(frame, (256, 256))[..., :3] for frame in driving_video]
+    
+    with open(config) as f:
+        config_ = yaml.load(f)
+    estimate_jacobian = config_['model_params']['common_params']['estimate_jacobian']
+    print(f'estimate jacobian: {estimate_jacobian}')
+
+    if find_best_frame_ or best_frame is not None:
+        i = best_frame if best_frame is not None else find_best_frame(source_image, driving_video, cpu=cpu)
+        print ("Best frame: " + str(i))
+        driving_forward = driving_video[i:]
+        driving_backward = driving_video[:(i+1)][::-1]
+        predictions_forward = make_animation(source_image, driving_forward, generator, kp_detector, he_estimator, relative=relative, adapt_movement_scale=adapt_scale, estimate_jacobian=estimate_jacobian, cpu=cpu, free_view=free_view, yaw=yaw, pitch=pitch, roll=roll)
+        predictions_backward = make_animation(source_image, driving_backward, generator, kp_detector, he_estimator, relative=relative, adapt_movement_scale=adapt_scale, estimate_jacobian=estimate_jacobian, cpu=cpu, free_view=free_view, yaw=yaw, pitch=pitch, roll=roll)
+        predictions = predictions_backward[::-1] + predictions_forward[1:]
+    else:
+        predictions = make_animation(source_image, driving_video, generator, kp_detector, he_estimator, relative=relative, adapt_movement_scale=adapt_scale, estimate_jacobian=estimate_jacobian, cpu=cpu, free_view=free_view, yaw=yaw, pitch=pitch, roll=roll)
+    
+    # save video
+    #output_path = 'asset/output'
+    output_path = '/data'
+    os.makedirs(output_path, exist_ok=True)
+    
+    print(f'{output_path}/{output_name}')    
+    
+    imageio.mimsave(f'{output_path}/{output_name}', [img_as_ubyte(frame) for frame in predictions], fps=fps)
+    
+    if audio:
+        audioclip = VideoFileClip(driving)
+        audio = audioclip.audio
+        videoclip = VideoFileClip(f'{output_path}/{output_name}')
+        videoclip.audio = audio
+        name = output_name.strip('.mp4')
+        videoclip.write_videofile(f'{output_path}/{name}_audio.mp4')
+        return f'./{output_path}/{name}_audio.mp4'
+    else:
+        return f'{output_path}/{output_name}'
--- docker/run.sh
+++ docker/run.sh
@@ -6,12 +6,14 @@ xhost +local:root
 docker run -it \
     --privileged \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="${PWD}/..:/One-Shot_Free-View_Neural_Talking_Head_Synthesis" \
     --volume="${PWD}/data:/data" \
     --volume="${PWD}/checkpoint:/checkpoint" \
     --rm \
     --gpus all \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
+    -p 8889:8889 \
     ghcr.io/donghee/one-shot-free-view-neural-talking-head-synthesis:cuda11.0.3-devel-ubuntu20.04 \
-    python3 /One-Shot_Free-View_Neural_Talking_Head_Synthesis/demo.py --result_video /data/result.mp4 --config /checkpoint/vox-256-spade.yaml --checkpoint /checkpoint/00000189-checkpoint.pth.zip --source_image /data/man.png --driving_video /data/man.mp4 --relative --adapt_scale
-#    bash
+    bash
+    #python3 /One-Shot_Free-View_Neural_Talking_Head_Synthesis/demo.py --result_video /data/result.mp4 --config /checkpoint/vox-256-spade.yaml --checkpoint /checkpoint/00000189-checkpoint.pth.zip --source_image /data/man.png --driving_video /data/man.mp4 --relative --adapt_scale
